580e1c7d5a032960bd3eaf7f0156b9ea7c69377c
Qubad786 <muhammadrehan69@gmail.com>
2018-08-09 21:10:53 +0500

    refactor

--- openedx/core/djangoapps/video_pipeline/config/waffle.py
+++ openedx/core/djangoapps/video_pipeline/config/waffle.py
@@ -17,7 +17,6 @@ def waffle_flags():
     """
     namespace = WaffleFlagNamespace(name=WAFFLE_NAMESPACE, log_prefix=u'Videos: ')
     return {
-        # By default, disable deprecate youtube flag. This can be enabled on a course-by-course basis.
         DEPRECATE_YOUTUBE: CourseWaffleFlag(
             waffle_namespace=namespace,
             flag_name=DEPRECATE_YOUTUBE
